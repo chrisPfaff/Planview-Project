ce617c92f41b5c0a5f3eabea8b0ab7aafd066f1e
Chris Pfaff <chrispfaff10@gmail.com>
2020-02-23 21:12:46 -0600

    added description

--- src/components/InteractDescription.js
+++ src/components/InteractDescription.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/InteractDescription.scss";
 
+//renders description container with text inside
 const InteractDescription = ({ content }) => {
   return (
     <section className="description-container">
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/Sidebar.scss";
 
+//renders a div that holds the title of the Track and content
 const SideBar = ({ title, list }) => {
   return (
     <div>
